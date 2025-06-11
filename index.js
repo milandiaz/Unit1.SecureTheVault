b738d36bfdf838529519f4333edc1c64d14967a0
milandiaz <milandiaz2000@hotmail.com>
2025-06-11 11:41:32 -0500

    First workshop8 commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//Declaring code1
+let code1;
+
+//Definign code1
+code1 = 7 + 3;
+
+//Initializing code2
+let code2 = 82 / 2 - 1;
+
+//Initializing code3
+let code3 = (13 % 10) * 13;
+
+//Initializing message
+let message = "The vault has been secured. The combination is: ";
+
+//Initializing codeA
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+//Initializing codeB
+let codeB = `${code1}-${code2}-${code3}`;
+
+//Printing message on console
+console.log(message);
+
+//Printing codeA value on console
+console.log(codeA);
+
+//Printing codeB value on console
+console.log(codeB);
